ab8ea469b69c2f63a4db105074856656db7dc408
Ranis07 <ranismk07@gmail.com>
2021-06-08 18:54:34 +0545

    length changed to 17

--- password_genrator.py
+++ password_genrator.py
@@ -6,7 +6,7 @@ sm_alpha = "abcdefghijk"
 lg_alpha = "ABCDEFGHIJK"
 
 all = symbols+numbers+sm_alpha+lg_alpha
-length =16
+length =17
 
 password = "".join(random.sample(all,length))
 print(password)
