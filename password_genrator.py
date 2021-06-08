7eaa6e95bb5dc8e0d075955a67681b75e70943c4
Ranis07 <ranismk07@gmail.com>
2021-06-08 18:37:44 +0545

    length updated

--- password_genrator.py
+++ password_genrator.py
@@ -6,7 +6,7 @@ sm_alpha = "abcdefghijk"
 lg_alpha = "ABCDEFGHIJK"
 
 all = symbols+numbers+sm_alpha+lg_alpha
-length = 6
+length =16
 
 password = "".join(random.sample(all,length))
-print(password)
+print(password)                                                                                    
