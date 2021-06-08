bad38260f95077c1d69e179d1cda243fa51c140f
Ranis07 <ranismk07@gmail.com>
2021-06-08 11:33:32 +0545

    password-generator

---
+++ password_genrator.py
@@ -0,0 +1,12 @@
+import random
+
+symbols = "!@#$%^&"
+numbers = "123456789"
+sm_alpha = "abcdefghijk"
+lg_alpha = "ABCDEFGHIJK"
+
+all = symbols+numbers+sm_alpha+lg_alpha
+length = 6
+
+password = "".join(random.sample(all,length))
+print(password)
